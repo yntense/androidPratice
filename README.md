3d437057b3f46fb3b9b66690528db989154cf9ef
LAPTOP-9APVM9RC\cai <18876309787@163.com>
2021-06-10 14:33:14 +0800

    1.添加一个 application 文件 2.创建一个基本项目

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# androidPratice
+目的：
+
+1. android 代码实践
+
+
+
+环境：
+
+- android studio 4.2.1
+
+  
+
---
+++ app/src/main/java/com/example/androidpractice/CPApplication.java
@@ -0,0 +1,36 @@
+package com.example.androidpractice;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * androidPractice 的 Application 入口。
+ * Created by yntense on 21/6/10.
+ */
+public class CPApplication extends Application {
+    private static final String TAG = "CPApplication";
+
+    private static Context context;
+
+    public static Context getContext(){ return context;};
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        Log.i(TAG, "onCreate:CPApplication ");
+    }
+}
+/**
+ *  skill：
+ *  1./** ctrl + enter 快速注释
+ *  function：
+ *  1.get context
+ *
+ *  notice：
+ *  1.需要在Manifests中的 application 标签中添加 android:name=".CPApplication"，
+ *  此文件才能在MAinActivity之前被调用
+ *
+ *
+ */
