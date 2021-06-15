6b3e4a05ef1f2529a371eb6610aba03d1d3fc82e
LAPTOP-9APVM9RC\cai <18876309787@163.com>
2021-06-15 18:37:02 +0800

    1.tab 应该和 viewpager 一起使用

--- app/src/main/java/com/example/androidpractice/fragment/home/HomeFragment.java
+++ app/src/main/java/com/example/androidpractice/fragment/home/HomeFragment.java
@@ -1,28 +1,65 @@
 package com.example.androidpractice.fragment.home;
 
+import android.graphics.Typeface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.androidpractice.R;
 import com.example.androidpractice.base.BaseFragment;
+import com.qmuiteam.qmui.util.QMUIDisplayHelper;
+import com.qmuiteam.qmui.widget.tab.QMUITab;
+import com.qmuiteam.qmui.widget.tab.QMUITabBuilder;
+import com.qmuiteam.qmui.widget.tab.QMUITabSegment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class HomeFragment extends BaseFragment {
+    private final static String TAG = HomeFragment.class.getSimpleName();
 
-    @BindView(R.id.pager)
-    ViewPager mViewPager;
+    @BindView(R.id.tabs)
+    QMUITabSegment mTabSegment;
     @Override
     protected View onCreateView() {
         FrameLayout layout = (FrameLayout) LayoutInflater.from(getActivity()).inflate(R.layout.fragment_home, null);
         ButterKnife.bind(this, layout);
-//        initTabs();
+        initTabs();
 //        initPagers();
         return layout;
+    }
+
+    private void initTabs() {
+
+        Log.i(TAG, "initTabs: ");
+        QMUITabBuilder builder = mTabSegment.tabBuilder();
+        builder.setTypeface(null, Typeface.DEFAULT_BOLD);
+        builder.setSelectedIconScale(1.2f)
+                .setTextSize(QMUIDisplayHelper.sp2px(getContext(), 13), QMUIDisplayHelper.sp2px(getContext(), 15))
+                .setDynamicChangeIconColor(false);
+        QMUITab component = builder
+                .setNormalDrawable(ContextCompat.getDrawable(getContext(), R.mipmap.icon_tabbar_component))
+                .setSelectedDrawable(ContextCompat.getDrawable(getContext(), R.mipmap.icon_tabbar_component_selected))
+                .setText("Components")
+                .build(getContext());
+        QMUITab util = builder
+                .setNormalDrawable(ContextCompat.getDrawable(getContext(), R.mipmap.icon_tabbar_util))
+                .setSelectedDrawable(ContextCompat.getDrawable(getContext(), R.mipmap.icon_tabbar_util_selected))
+                .setText("Helper")
+                .build(getContext());
+        QMUITab lab = builder
+                .setNormalDrawable(ContextCompat.getDrawable(getContext(), R.mipmap.icon_tabbar_lab))
+                .setSelectedDrawable(ContextCompat.getDrawable(getContext(), R.mipmap.icon_tabbar_lab_selected))
+                .setText("Lab")
+                .build(getContext());
+
+        mTabSegment.addTab(component)
+                .addTab(util)
+                .addTab(lab);
     }
 
 
