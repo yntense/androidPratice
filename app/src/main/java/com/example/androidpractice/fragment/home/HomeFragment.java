5a4acf70a05b2323c3c678229dcb50b2935d5ff7
LAPTOP-9APVM9RC\cai <18876309787@163.com>
2021-06-16 10:25:06 +0800

    1.成功使用QMUI的viewpager

--- app/src/main/java/com/example/androidpractice/fragment/home/HomeFragment.java
+++ app/src/main/java/com/example/androidpractice/fragment/home/HomeFragment.java
@@ -4,9 +4,11 @@ import android.graphics.Typeface;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.androidpractice.R;
@@ -16,20 +18,69 @@ import com.qmuiteam.qmui.widget.tab.QMUITab;
 import com.qmuiteam.qmui.widget.tab.QMUITabBuilder;
 import com.qmuiteam.qmui.widget.tab.QMUITabSegment;
 
+import java.util.HashMap;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class HomeFragment extends BaseFragment {
     private final static String TAG = HomeFragment.class.getSimpleName();
 
+    @BindView(R.id.pager)
+    ViewPager mViewPager;
     @BindView(R.id.tabs)
     QMUITabSegment mTabSegment;
+
+    private HashMap<Pager, HomeController> mPages;
+    //页面适配器
+    private PagerAdapter mPagerAdapter = new PagerAdapter() {
+
+        private int mChildCount = 0;
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public int getCount() {
+            return mPages.size();
+        }
+
+        @Override
+        public Object instantiateItem(final ViewGroup container, int position) {
+            HomeController page = mPages.get(Pager.getPagerFromPositon(position));
+            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            container.addView(page, params);
+            return page;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            if (mChildCount == 0) {
+                return POSITION_NONE;
+            }
+            return super.getItemPosition(object);
+        }
+
+        @Override
+        public void notifyDataSetChanged() {
+            mChildCount = getCount();
+            super.notifyDataSetChanged();
+        }
+    };
+
     @Override
     protected View onCreateView() {
         FrameLayout layout = (FrameLayout) LayoutInflater.from(getActivity()).inflate(R.layout.fragment_home, null);
         ButterKnife.bind(this, layout);
         initTabs();
-//        initPagers();
+        initPagers();
         return layout;
     }
 
@@ -62,5 +113,48 @@ public class HomeFragment extends BaseFragment {
                 .addTab(lab);
     }
 
+    private void initPagers() {
+
+        HomeController.HomeControlListener listener = new HomeController.HomeControlListener() {
+            @Override
+            public void startFragment(BaseFragment fragment) {
+                HomeFragment.this.startFragment(fragment);
+            }
+        };
+
+        mPages = new HashMap<>();
+
+        HomeController homeComponentsController = new HomeComponentsController(getActivity());
+        homeComponentsController.setHomeControlListener(listener);
+        mPages.put(Pager.COMPONENT, homeComponentsController);
+
+        HomeController homeUtilController = new HomeUtilController(getActivity());
+        homeUtilController.setHomeControlListener(listener);
+        mPages.put(Pager.UTIL, homeUtilController);
+
+        HomeController homeLabController = new HomeLabController(getActivity());
+        homeLabController.setHomeControlListener(listener);
+        mPages.put(Pager.LAB, homeLabController);
+
+        mViewPager.setAdapter(mPagerAdapter);
+        mTabSegment.setupWithViewPager(mViewPager, false);
+    }
+
+    enum Pager {
+        COMPONENT, UTIL, LAB;
+
+        public static Pager getPagerFromPositon(int position) {
+            switch (position) {
+                case 0:
+                    return COMPONENT;
+                case 1:
+                    return UTIL;
+                case 2:
+                    return LAB;
+                default:
+                    return COMPONENT;
+            }
+        }
+    }
 
 }
